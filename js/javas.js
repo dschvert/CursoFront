7e163b64bc4a4baca0a92ee99d3a046edf6ba890
dschvert <31259358+dschvert@users.noreply.github.com>
2023-11-06 12:04:35 -0300

    Add files via upload

---
+++ js/javas.js
@@ -0,0 +1,13 @@
+//modo nocturno
+document.getElementById("btnnocturno").addEventListener("click", function ()  {
+ document.body.style.backgroundColor="black" ;
+ document.getElementById("titulopag").style.color="white" ;
+ document.getElementById("subreceta").style.color="white" ;
+ console.log("Entro")
+});
+
+
+//alerta datos dev
+document.getElementById("btndev").addEventListener("click", function ()  {
+ alert ("Desarrollador:\n\nDeborah Schvert\nContacto: 2914222838\ndschvert@gmail.com");
+});
